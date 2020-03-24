ebfec3b66dbaa9a71ac036b2f62b85833800a12d
yelqgd <yelqgd@gmail.com>
2020-03-24 21:02:37 +0800

    init with express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
-let x = require('http')
+const express = require('express')
+const port = 3333
 
-console.log('init')
-console.log(x)
+function startServer() {
+  const app = express()
+
+  app.listen(port, (err: any) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+      return
+    }
+    console.info(`
+      ################################################
+      🛡️  Server listening on port: ${port} 🛡️ 
+      ################################################
+    `)
+  })
+}
+
+startServer()
